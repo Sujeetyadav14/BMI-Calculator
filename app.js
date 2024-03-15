ca1ea8eec20b9bd49a2d3756dbe0c859f9eac5cf
Sujeet Yadav <143320694+Sujeetyadav14@users.noreply.github.com>
2024-03-15 12:02:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.getElementById("Height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+  const result = document.getElementById("results");
+  if (height === "" || height < 0 || isNaN(height)) {
+    result.innerHTML = `Enter the valid Number ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `Enter the valid Number ${weight}`;
+  } else {
+    const bmi = (weight / ((height*height)/10000)).toFixed(2);
+    result.innerHTML = `<span>${bmi}</span>`
+  }
+
+  
+});
